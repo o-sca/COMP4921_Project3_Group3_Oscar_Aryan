5eebcf2cb7e3b2e0ae831ee57e446177e625a66f
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-21 17:31:36 -0800

    refactor(filter): include json

--- server/src/common/filters/not-found.filter.ts
+++ server/src/common/filters/not-found.filter.ts
@@ -10,6 +10,8 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
   catch(_exception: NotFoundException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    return response.status(404);
+    return response.status(404).json({
+      statusCode: 404,
+    });
   }
 }
