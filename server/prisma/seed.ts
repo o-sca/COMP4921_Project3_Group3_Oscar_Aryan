d4eebae89170724a6dfb0ec87eb607366aefda02
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-28 18:13:07 -0800

    refactor(seed): more dummy users

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,7 +3,9 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const dummy = await prisma.user.upsert({
+  await prisma.user.deleteMany({});
+
+  await prisma.user.upsert({
     where: { email: 'dummy@mail.com' },
     update: {},
     create: {
@@ -16,8 +18,8 @@ async function main() {
     },
   });
 
-  const oscar = await prisma.user.upsert({
-    where: { email: 'oscar@gmail.com' },
+  await prisma.user.upsert({
+    where: { email: 'oscar@mail.com' },
     update: {},
     create: {
       email: 'oscar@mail.com',
@@ -29,7 +31,7 @@ async function main() {
     },
   });
 
-  const aryan = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'aryan@mail.com' },
     update: {},
 
@@ -43,10 +45,61 @@ async function main() {
     },
   });
 
-  console.log({ dummy, oscar, aryan });
+  await prisma.user.createMany({
+    data: [
+      {
+        email: 'dummy2@mail.com',
+        first_name: 'Carmen',
+        last_name: 'Berzatto',
+        password:
+          '$2a$12$.Bgf65iUQLngvowMdhMC.eURFSJhPCBcjXkfHb79OBTqzF/lAtvvS',
+        profile_pic_url:
+          'https://res.cloudinary.com/dj6rvkhvo/image/upload/v1697499455/avatars/1/h1hhribuj7xiffrndplr.jpg',
+      },
+      {
+        email: 'dummy3@mail.com',
+        first_name: 'Mikey',
+        last_name: 'Berzatto',
+        password:
+          '$2a$12$.Bgf65iUQLngvowMdhMC.eURFSJhPCBcjXkfHb79OBTqzF/lAtvvS',
+        profile_pic_url:
+          'https://res.cloudinary.com/dj6rvkhvo/image/upload/v1697499455/avatars/1/h1hhribuj7xiffrndplr.jpg',
+      },
+      {
+        email: 'dummy4@mail.com',
+        first_name: 'Richard',
+        last_name: 'Jerimovic',
+        password:
+          '$2a$12$.Bgf65iUQLngvowMdhMC.eURFSJhPCBcjXkfHb79OBTqzF/lAtvvS',
+        profile_pic_url:
+          'https://res.cloudinary.com/dj6rvkhvo/image/upload/v1697499455/avatars/1/h1hhribuj7xiffrndplr.jpg',
+      },
+      {
+        email: 'dummy5@mail.com',
+        first_name: 'Ayo',
+        last_name: 'Edebiri',
+        password:
+          '$2a$12$.Bgf65iUQLngvowMdhMC.eURFSJhPCBcjXkfHb79OBTqzF/lAtvvS',
+        profile_pic_url:
+          'https://res.cloudinary.com/dj6rvkhvo/image/upload/v1697499455/avatars/1/h1hhribuj7xiffrndplr.jpg',
+      },
+      {
+        email: 'dummy6@mail.com',
+        first_name: 'Jim',
+        last_name: 'Halpert',
+        password:
+          '$2a$12$.Bgf65iUQLngvowMdhMC.eURFSJhPCBcjXkfHb79OBTqzF/lAtvvS',
+        profile_pic_url:
+          'https://res.cloudinary.com/dj6rvkhvo/image/upload/v1697499455/avatars/1/h1hhribuj7xiffrndplr.jpg',
+      },
+    ],
+  });
 }
 
 main()
+  .then(() => {
+    console.log('seeded');
+  })
   .catch(async (e) => {
     console.error(e);
   })
